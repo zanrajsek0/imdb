da60f8cc9ee86693dc4416aaf4887316d9632379
zanrajsek0 <zanrajsek0@gmail.com>
2021-04-09 09:16:07 +0200

    Dodan PDO in add v bazo

--- user_insert.php
+++ user_insert.php
@@ -1,5 +1,24 @@
 <?php
-echo '<pre>';
-print_r($_POST);
-echo '</pre>';
+include_once "db.php";
+
+$first_name=$_POST['first_name'];
+$last_name=$_POST['last_name'];
+
+$email=$_POST['email'];
+$pass1=$_POST['pass1'];
+$pass2=$_POST['pass2'];
+
+//Preverim so podatki polni
+if(!empty($first_name) && !empty($last_name) && !empty($email) && !empty($pass1) && ($pass1==$pass2)){
+    $pass=password_hash($pass1,PASSWORD_DEFAULT);
+    $query="INSERT INTO users(first_name,last_name,email,pass) VALUES(?,?,?,?)";
+    $stmt=$pdo->prepare($query);
+    $stmt->execute([$first_name,$last_name,$email,$pass]);
+
+    header("Location: login.php"); die();
+
+
+}else{
+    header("Location: user_add.php"); die();
+}
 ?>
